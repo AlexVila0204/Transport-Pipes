f58ce9016713afd5e609a38a13e15e42ad3cdd0d
AlexVila0204 <godoyalberth.0204@gmail.com>
2026-08-18 16:20:00 -0600

    fix: remove duplicate 1.21.4/1.21.5 module and case entries

--- transportpipes-core/src/main/java/de/robotricker/transportpipes/TransportPipes.java
+++ transportpipes-core/src/main/java/de/robotricker/transportpipes/TransportPipes.java
@@ -175,25 +175,9 @@ public class TransportPipes extends JavaPlugin {
                     Bukkit.getLogger().log(Level.SEVERE, "TransportPipes could not find a valid implementation for this server version.");
                 }
                 break;
-            case "1.21.4":
-                try {
-                    protocolProvider = (ProtocolProvider) Class.forName(protocolProviderClassName + "1_21_4").getDeclaredConstructor().newInstance();
-                    fakeBlockClass = Class.forName(fakeBlockClassName + "1_21_4");
-                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
-                    Bukkit.getLogger().log(Level.SEVERE, "TransportPipes could not find a valid implementation for this server version.");
-                }
-                break;
-            case "1.21.5":
-                try {
-                    protocolProvider = (ProtocolProvider) Class.forName(protocolProviderClassName + "1_21_5").getDeclaredConstructor().newInstance();
-                    fakeBlockClass = Class.forName(fakeBlockClassName + "1_21_5");
-                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
-                    Bukkit.getLogger().log(Level.SEVERE, "TransportPipes could not find a valid implementation for this server version.");
-                }
-                break;
             default:
                 getLogger().log(Level.SEVERE, "------------------------------------------");
-                getLogger().log(Level.SEVERE, "TransportPipes currently only works with Minecraft 1.16.5 through 1.21.5. You are running version " + version + ".");
+                getLogger().log(Level.SEVERE, "TransportPipes currently only works with Minecraft 1.16.5 through 1.21.5 You are running version " + version + ".");
                 getLogger().log(Level.SEVERE, "------------------------------------------");
                 Bukkit.getPluginManager().disablePlugin(this);
                 return;
