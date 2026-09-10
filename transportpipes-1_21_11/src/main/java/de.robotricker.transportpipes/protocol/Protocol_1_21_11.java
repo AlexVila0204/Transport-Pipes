ee35d2c276c2f8d56db01dfc4f15dd9195bd8bc9
AlexVila0204 <godoyalberth.0204@gmail.com>
2026-09-10 16:15:00 -0600

    feat: add support for Minecraft 1.21.11 (v1_21_R7)

---
+++ transportpipes-1_21_11/src/main/java/de.robotricker.transportpipes/protocol/Protocol_1_21_11.java
@@ -0,0 +1,129 @@
+package de.robotricker.transportpipes.protocol;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.reflect.StructureModifier;
+import com.comphenix.protocol.wrappers.Vector3F;
+import com.comphenix.protocol.wrappers.WrappedDataValue;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+import com.google.common.collect.Lists;
+import de.robotricker.transportpipes.TransportPipes;
+import de.robotricker.transportpipes.duct.Duct;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.AmethystCluster;
+import org.bukkit.block.data.type.LightningRod;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Protocol_1_21_11 implements ProtocolProvider {
+
+    @Override
+    public int getMaskIndex() {
+        return 15;
+    }
+
+    @Override
+    public int getHeadRotIndex() {
+        return 16;
+    }
+
+    @Override
+    public int getRightArmRotIndex() {
+        return 19;
+    }
+
+    @Override
+    public void removeASD(Player p, List<ArmorStandData> armorStandData, ProtocolManager protocolManager) {
+        PacketContainer entityDestroyContainer = protocolManager.createPacket(PacketType.Play.Server.ENTITY_DESTROY);
+        List<Integer> ids = armorStandData.stream().mapToInt(ArmorStandData::getEntityID).boxed().collect(Collectors.toList());
+        entityDestroyContainer.getIntLists().write(0, ids);
+        protocolManager.sendServerPacket(p, entityDestroyContainer);
+    }
+
+    @Override
+    public Recipe calculateRecipe(TransportPipes transportPipes, Inventory inventory, Duct duct) {
+        ItemStack[] craftingMatrix = new ItemStack[9];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (inventory.getItem(10 + row * 9 + col) != null) {
+                    craftingMatrix[row * 3 + col] = inventory.getItem(10 + row * 9 + col);
+                } else {
+                    craftingMatrix[row * 3 + col] = new ItemStack(Material.AIR);
+                }
+            }
+        }
+
+        return Bukkit.getCraftingRecipe(craftingMatrix, duct.getWorld());
+    }
+
+    @Override
+    public StructureModifier setASDYaw(PacketContainer spawnEntityLivingContainer, double yaw) {
+        return spawnEntityLivingContainer.getBytes().write(1, (byte) (yaw * 256 / 360));
+    }
+
+    @Override
+    public boolean isClickedFaceDirectional(BlockData blockData) {
+        clickedFaceDirectionals.add(AmethystCluster.class);
+        clickedFaceDirectionals.add(LightningRod.class);
+        clickedFaceMaterials.add(Material.DEEPSLATE);
+        clickedFaceMaterials.add(Material.INFESTED_DEEPSLATE);
+        clickedFaceMaterials.add(Material.OCHRE_FROGLIGHT);
+        clickedFaceMaterials.add(Material.PEARLESCENT_FROGLIGHT);
+        clickedFaceMaterials.add(Material.VERDANT_FROGLIGHT);
+        clickedFaceMaterials.add(Material.MUDDY_MANGROVE_ROOTS);
+        return ProtocolProvider.super.isClickedFaceDirectional(blockData);
+    }
+
+    @Override
+    public PacketContainer setEntityMetadata(ProtocolManager protocolManager, ArmorStandData asd) {
+        PacketContainer entityMetadataContainer = protocolManager.createPacket(PacketType.Play.Server.ENTITY_METADATA);
+        entityMetadataContainer.getModifier().writeDefaults();
+        entityMetadataContainer.getIntegers().write(0, asd.getEntityID());
+
+        byte bitMask = (byte) ((asd.isSmall() ? 0x01 : 0x00) | 0x04 | 0x08 | 0x10);
+
+        List<WrappedDataValue> wrappedDataValueList = Lists.newArrayList();
+
+        wrappedDataValueList.add(new WrappedDataValue(0, WrappedDataWatcher.Registry.get(Byte.class), (byte) (0x20 | 0x01)));
+
+        wrappedDataValueList.add(new WrappedDataValue(3, WrappedDataWatcher.Registry.get(Boolean.class), false));
+
+        wrappedDataValueList.add(new WrappedDataValue(getMaskIndex(), WrappedDataWatcher.Registry.get(Byte.class), bitMask));
+
+        Vector3F headRotation = new Vector3F(
+                (float) asd.getHeadRotation().getX(),
+                (float) asd.getHeadRotation().getY(),
+                (float) asd.getHeadRotation().getZ()
+        );
+        wrappedDataValueList.add(new WrappedDataValue(
+                getHeadRotIndex(),
+                WrappedDataWatcher.Registry.getVectorSerializer(),
+                Vector3F.getConverter().getGeneric(headRotation)
+        ));
+
+        Vector3F armRotation = new Vector3F(
+                (float) asd.getArmRotation().getX(),
+                (float) asd.getArmRotation().getY(),
+                (float) asd.getArmRotation().getZ()
+        );
+        wrappedDataValueList.add(new WrappedDataValue(
+                getRightArmRotIndex(),
+                WrappedDataWatcher.Registry.getVectorSerializer(),
+                Vector3F.getConverter().getGeneric(armRotation)
+        ));
+
+        entityMetadataContainer.getDataValueCollectionModifier().write(0, wrappedDataValueList);
+
+        return entityMetadataContainer;
+    }
+
+}
--- transportpipes-core/src/main/java/de/robotricker/transportpipes/TransportPipes.java
+++ transportpipes-core/src/main/java/de/robotricker/transportpipes/TransportPipes.java
@@ -194,6 +194,14 @@ public class TransportPipes extends JavaPlugin {
                     Bukkit.getLogger().log(Level.SEVERE, "TransportPipes could not find a valid implementation for this server version.");
                 }
                 break;
+            case "1.21.11":
+                try {
+                    protocolProvider = (ProtocolProvider) Class.forName(protocolProviderClassName + "1_21_11").getDeclaredConstructor().newInstance();
+                    fakeBlockClass = Class.forName(fakeBlockClassName + "1_21_11");
+                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
+                    Bukkit.getLogger().log(Level.SEVERE, "TransportPipes could not find a valid implementation for this server version.");
+                }
+                break;
             default:
                 getLogger().log(Level.SEVERE, "------------------------------------------");
                 getLogger().log(Level.SEVERE, "TransportPipes currently only works with Minecraft 1.16.5 through 1.21.10 You are running version " + version + ".");
